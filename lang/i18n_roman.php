6eb6077fa32821e7f99e3ba500c9d730c1f37729
Joan Miquel Torres <joanmi@bitifet.net>
2012-08-09 03:46:09 +0200

    Roman numeration support.
    Quite big but not yet full range support.

---
+++ lang/i18n_roman.php
@@ -0,0 +1,73 @@
+<?php
+
+// Catalan:
+// =======
+
+// Spain:
+$r['roman'] = array ( // Default / male:/*{{{*/
+/*
+	'n00' => '',
+	'n000' => '',
+	'n0000' => '',
+	'n00000' => '',
+	'n000000' => '',
+	'n0000000' => '',
+	'n00000000' => '',
+*/
+
+	'n0' => '',
+	'n1' => 'I',
+	'n2' => 'II',
+	'n3' => 'III',
+	'n4' => 'IV',
+	'n5' => 'V',
+	'n6' => 'VI',
+	'n7' => 'VII',
+	'n8' => 'VIII',
+	'n9' => 'IX',
+
+	'n1x' => 'X',
+	'n2x' => 'XX',
+	'n3x' => 'XXX',
+	'n4x' => 'XL',
+	'n5x' => 'L',
+	'n6x' => 'LX',
+	'n7x' => 'LXX',
+	'n8x' => 'LXXX',
+	'n9x' => 'XC',
+
+	'n1xx' => 'C',
+	'n2xx' => 'CC',
+	'n3xx' => 'CCC',
+	'n4xx' => 'CD',
+	'n5xx' => 'D',
+	'n6xx' => 'DC',
+	'n7xx' => 'DCC',
+	'n8xx' => 'DCCC',
+	'n9xx' => 'CM',
+
+
+	'n1xxx' => 'M',
+	'n2xxx' => 'MM',
+	'n3xxx' => 'MMM',
+	'n4xxx' => 'Mv',  // FIXME 'v' -> "V" covered by '-'
+	'n5xxx' => 'v',
+	'n6xxx' => 'vM',
+	'n7xxx' => 'vMM',
+	'n8xxx' => 'v',
+	'n9xxx' => 'Mx',  // FIXME 'x' -> 'X' covered by '-'
+
+
+	'n1xxxx' => 'x',
+	'n2xxxx' => 'xx',
+	'n3xxxx' => 'xxx',
+	'n4xxxx' => 'xl',  // FIXME 'l' -> 'L' covered by '-'
+	'n5xxxx' => 'l',
+	'n6xxxx' => 'lx',
+	'n7xxxx' => 'lxx',
+	'n8xxxx' => 'lxxx',
+	'n9xxxx' => 'xc',  //FIXME 'c' -> 'C' covered by '-'
+
+);/*}}}*/
+
+return $r;
