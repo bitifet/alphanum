85843a24610f6588a5ae1d98f09d9ae8a037a142
joanmi <joanmi@boreal.(none)>
2012-08-16 06:15:02 +0200

    'Stupid' ident language (demo).

---
+++ lib/lang/i18n_ident.php
@@ -0,0 +1,31 @@
+<?php
+
+// Ident: (Demo)
+// =====
+
+// Stupid 'ident' language which does almost nothing.
+// ...but which demostrates how alphanum can recode numbers
+// to its original form ...or recode it (you can try to
+// mutate any digits to arbitrary other digits or characters...
+
+
+$r['ident'] = array ( // Default / male:/*{{{*/
+
+	'n0' => '0',
+	'n1' => '1',
+	'n2' => '2',
+	'n3' => '3',
+	'n4' => '4',
+	'n5' => '5',
+	'n6' => '6',
+	'n7' => '7',
+	'n8' => '8',
+	'n9' => '9',
+
+	'nxx' => '',
+
+);/*}}}*/
+
+
+
+return $r;
